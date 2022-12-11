370bb52a5f613abb495ff1dca00b101dbe0fade5
Alia Peterson <70297733+alia-peterson@users.noreply.github.com>
2022-12-10 17:00:43 -0700

    Refactor part 9 functions

--- 2022/src/09_rope-bridge/index.ts
+++ 2022/src/09_rope-bridge/index.ts
@@ -9,100 +9,58 @@ R 2`;
 
 import { movements } from "./data";
 
+const movementMap = {
+  R: { move: "+", axis: 0 },
+  D: { move: "-", axis: 1 },
+  L: { move: "-", axis: 0 },
+  U: { move: "+", axis: 1 },
+};
+
 interface IHandleMoves {
-  acc: { H: number[]; T: number[] };
-  movement: "+" | "-";
-  direction: 0 | 1;
-  tailPositions: number[][];
+  acc: { H: number[]; T: number[]; TP: number[][] };
+  direction: "R" | "L" | "U" | "D";
   amount: number;
 }
 
-const handleMove = ({
-  acc,
-  movement,
-  direction,
-  tailPositions,
-  amount,
-}: IHandleMoves) => {
-  const { H, T } = acc;
-  const otherDir = direction === 0 ? 1 : 0;
-  const dMove = movement === "+" ? 1 : -1;
+const handleMove = ({ acc, direction, amount }: IHandleMoves) => {
+  const { H, T, TP } = acc;
+  const { axis, move } = movementMap[direction];
+  const otherDir = axis === 0 ? 1 : 0;
+  const dMove = move === "+" ? 1 : -1;
 
   for (let i = 0; i < amount; i++) {
-    if (
-      H[direction] !== T[direction] &&
-      H[direction] + dMove !== T[direction]
-    ) {
-      T[direction] = T[direction] + dMove;
+    if (H[axis] !== T[axis] && H[axis] + dMove !== T[axis]) {
+      T[axis] = T[axis] + dMove;
       T[otherDir] = H[otherDir];
     }
 
     const tCopy = Array.from(T);
-    const found = tailPositions.find(
-      (pos) => pos[0] === tCopy[0] && pos[1] === tCopy[1]
-    );
+    const found = TP.find((pos) => pos[0] === tCopy[0] && pos[1] === tCopy[1]);
 
     if (!found) {
-      tailPositions.push(tCopy);
+      TP.push(tCopy);
     }
 
-    H[direction] = H[direction] + dMove;
+    H[axis] = H[axis] + dMove;
   }
 };
 
 const calculateMovements = (input: string) => {
   const moves = input.split("\n");
-  const tailPositions: number[][] = [];
 
   const output = moves.reduce(
     (acc, move) => {
-      const [direction, num] = move.split(" ");
+      const [dir, num] = move.split(" ");
+      const direction = dir as IHandleMoves["direction"];
       const amount = parseInt(num);
-      switch (direction) {
-        case "R":
-          handleMove({
-            acc,
-            movement: "+",
-            direction: 0,
-            tailPositions,
-            amount,
-          });
-          break;
-        case "U":
-          handleMove({
-            acc,
-            movement: "+",
-            direction: 1,
-            tailPositions,
-            amount,
-          });
-          break;
-        case "L":
-          handleMove({
-            acc,
-            movement: "-",
-            direction: 0,
-            tailPositions,
-            amount,
-          });
-          break;
 
-        default:
-          handleMove({
-            acc,
-            movement: "-",
-            direction: 1,
-            tailPositions,
-            amount,
-          });
-          break;
-      }
+      handleMove({ acc, direction, amount });
       return acc;
     },
-    { H: [0, 0], T: [0, 0] }
+    { H: [0, 0], T: [0, 0], TP: [] }
   );
 
-  console.log(tailPositions.length);
+  console.log(output.TP.length);
 };
 
 calculateMovements(movements);
